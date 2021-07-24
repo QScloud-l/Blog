7cf31205c0bec6cad9cd6f6e1f031f110732d28a
lhq <lhq541381890@mail.ustc.edu.cn>
2021-07-25 00:29:33 +0800

    index

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('',log),
     path('', include(('users.urls', 'users'), namespace='users')),
+    path('', include(('home.urls','home'),namespace='home')),
 ]
